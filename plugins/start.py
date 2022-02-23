9e26d331fac05f39634aa0a11d340802a52ae090
Bdrl <93611653+Yansaii@users.noreply.github.com>
2022-02-23 22:26:42 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -117,12 +117,12 @@ async def start_command(client: Client, message: Message):
         buttons = [
             [InlineKeyboardButton("🔅 ᴛᴇɴᴛᴀɴɢ sᴀʏᴀ 🔅", callback_data="about")],
             [
-                InlineKeyboardButton("♻️𝗖𝗛𝗔𝗡𝗡𝗘𝗟♻️", url=client.invitelink),
-                InlineKeyboardButton("♻️𝗚𝗥𝗢𝗨𝗣♻️", url=client.invitelink2),
+                InlineKeyboardButton("♻𝗖𝗛𝗔𝗡𝗡𝗘𝗟¹♻", url=client.invitelink),
+                InlineKeyboardButton("♻𝗚𝗥𝗢𝗨𝗣♻²", url=client.invitelink2),
             ],
             [
-                InlineKeyboardButton("♻️𝗖𝗛𝗔𝗡𝗡𝗘𝗟♻️", url=client.invitelink3),
-                InlineKeyboardButton("♻️𝗚𝗥𝗢𝗨𝗣♻️", url=client.invitelink4), 
+                InlineKeyboardButton("♻𝗖𝗛𝗔𝗡𝗡𝗘𝗟♻³", url=client.invitelink3),
+                InlineKeyboardButton("♻𝗚𝗥𝗢𝗨𝗣♻⁴", url=client.invitelink4), 
             ],  
             [
                 InlineKeyboardButton("🔅 ᴛᴜᴛᴜᴘ 🔅", callback_data="close"),
