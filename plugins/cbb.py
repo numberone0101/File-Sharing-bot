4cb6e5fc9995c44db99f89c594c47cb460f38cce
Bdrl <93611653+Yansaii@users.noreply.github.com>
2022-02-23 12:19:02 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -14,7 +14,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Source Code: <a href='https://github.com/Zaen-ubot/File-Sharing'>Klik Disini</a></b>\n",
+            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Link Bokep: <a href='www.xnxx.com'>Klik Disini</a></b>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("🔅 ᴛᴜᴛᴜᴘ 🔅", callback_data="close")]]
